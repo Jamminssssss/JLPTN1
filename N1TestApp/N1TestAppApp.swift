60f9fef5385a98e861f0569beece7ae0276f5ff7
박재민 <benibara3758@gmail.com>
2025-12-22 20:04:59 +0900

    Initial Commit

---
+++ N1TestApp/N1TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  N1TestAppApp.swift
+//  N1TestApp
+//
+//  Created by JeaminPark on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct N1TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
